a57d0ffe079347d5e885ee604deb9f935566c4b9
Pholippo <philippgrabner77@gmail.com>
2025-06-12 21:38:28 +0200

    Update projekt_hackerdesign.py

--- projekt_hackerdesign.py
+++ projekt_hackerdesign.py
@@ -8,9 +8,9 @@ from openai import OpenAI
 from streamlit_lottie import st_lottie
 import requests
 
-# HIER MUSS ES STEHEN:
-client = OpenAI(api_key="sk-...")   # <- Diese Zeile musst du so anpassen:
 import os
+
+
 client = OpenAI(api_key=os.environ["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"])
 
 # --- HACKER-THEME: Dark Mode + Matrix Green + Monospace
